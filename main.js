492428c2a0f51633ff345e3435802929eabd4e22
Cayde Kim <cloudnine0219@gmail.com>
2023-07-12 18:40:31 +1200

    change name

--- main.js
+++ main.js
@@ -1,101 +1,99 @@
-'use strict';
+"use strict";
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector('#navbar');
+const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
+document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
-    navbar.classList.add('navbar--dark');
+    navbar.classList.add("navbar--dark");
   } else {
-    navbar.classList.remove('navbar--dark');
+    navbar.classList.remove("navbar--dark");
   }
 });
 
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event) => {
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-  navbarMenu.classList.remove('open');
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
 });
 
 // Navbar toggle button for small screen
-const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
-navbarToggleBtn.addEventListener('click', () => {
-  navbarMenu.classList.toggle('open');
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
 });
 
 // handel click on 'contact me' button on home
-const homeContactBtn = document.querySelector('.home__contact');
-homeContactBtn.addEventListener('click', () => {
-  scrollIntoView('#contact');
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
 });
 
 // Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector('.home__container');
+const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
+document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 // Show "arrow up" button when scrolling down
-const arrowUp = document.querySelector('.arrow-up');
-document.addEventListener('scroll', () => {
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
-    arrowUp.classList.add('visible');
+    arrowUp.classList.add("visible");
   } else {
-    arrowUp.classList.remove('visible');
+    arrowUp.classList.remove("visible");
   }
 });
 
 // Handle click on the "arrow up" button
-arrowUp.addEventListener('click', () => {
-  scrollIntoView('#home');
+arrowUp.addEventListener("click", () => {
+  scrollIntoView("#home");
 });
 
 // Projects
-const workBtnContainer = document.querySelector('.work__categories');
-const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (e) => {
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
   const filter = e.target.dataset.filter;
   if (filter == null) {
     return;
   }
 
   // Remove selection from the previous item and select the new one
-  const active = document.querySelector('.category__btn.selected');
+  const active = document.querySelector(".category__btn.selected");
   if (active != null) {
-    active.classList.remove('selected');
+    active.classList.remove("selected");
   }
-  e.target.classList.add('selected');
+  e.target.classList.add("selected");
 
-  projectContainer.classList.add('anim-out');
+  projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach((project) => {
       console.log(project.dataset.type);
-      if (filter === '*' || filter === project.dataset.type) {
-        project.classList.remove('invisible');
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
       } else {
-        project.classList.add('invisible');
+        project.classList.add("invisible");
       }
     });
-    projectContainer.classList.remove('anim-out');
+    projectContainer.classList.remove("anim-out");
   }, 300);
 });
 
-
-
 const sectionIds = [
-  '#home',
-  '#about',
-  '#skills',
-  '#work',
-  '#testimonials',
-  '#contact',
+  "#home",
+  "#about",
+  "#skills",
+  "#work",
+  "#testimonials",
+  "#contact",
 ];
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
@@ -105,27 +103,27 @@ const navItems = sectionIds.map((id) =>
 let selectedNavIndex = 0;
 let selectedNavItem = navItems[0];
 function selectNavItem(selected) {
-  selectedNavItem.classList.remove('active');
+  selectedNavItem.classList.remove("active");
   selectedNavItem = selected;
-  selectedNavItem.classList.add('active');
+  selectedNavItem.classList.add("active");
 }
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
+  scrollTo.scrollIntoView({ behavior: "smooth" });
   selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
 
 const observerOptions = {
   root: null,
-  rootMargin: '0px',
+  rootMargin: "0px",
   threshold: 0.5,
 };
 
 const observerCallback = (entries, observer) => {
   entries.forEach((entry) => {
     if (!entry.isIntersecting && entry.intersectionRatio > 0) {
-      console.log('y');
+      console.log("y");
       const index = sectionIds.indexOf(`#${entry.target.id}`);
       // 스크롤링이 아래로 되어서 페이지가 올라옴
       if (entry.boundingClientRect.y < 0) {
@@ -140,7 +138,7 @@ const observerCallback = (entries, observer) => {
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 sections.forEach((section) => observer.observe(section));
 
-window.addEventListener('wheel', () => {
+window.addEventListener("wheel", () => {
   if (window.scrollY === 0) {
     selectedNavIndex = 0;
   } else if (
@@ -151,4 +149,3 @@ window.addEventListener('wheel', () => {
   }
   selectNavItem(navItems[selectedNavIndex]);
 });
-
